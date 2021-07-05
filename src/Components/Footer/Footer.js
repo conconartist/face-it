422271867abb7c658f1e5c99337ad9f3be53bcee
conconartist <conconart.info@gmail.com>
2021-07-04 18:07:55 -0600

    Update footer

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -8,16 +8,8 @@ import './Footer.css';
 const Footer = () => {
   return (
     <footer>
-      <h4>FaceIt</h4>
-      <h5>is brought to you by</h5>
+      <h5>This version of FaceIt is brought to you by</h5>
         <div className='dev-container'>
-          <div className='dev'>
-            <p>Kelsie Besinger Yeh</p>
-              <div className='links'>
-                <a href='https://github.com/kelsiebesingeryeh/'><img src={github} alt='Kelsie GitHub' /></a>
-                <a href='https://www.linkedin.com/in/kelsie-besinger-yeh-80947132/'><img src={linkedin} alt='Kelsie LinkedIn' /></a>
-              </div>
-          </div>
           <div className='dev'>
             <p>Connie Hong </p>
               <div className='links'>
@@ -26,14 +18,11 @@ const Footer = () => {
                 <a href='https://twitter.com/conconartist?ref_src=twsrc%5Etfw' class='twitter-follow-button' data-show-count='false'><img src={twitter} alt='Connie Twitter' /></a>
               </div>
           </div>
-          <div className='dev'>
-            <p>Gabrielle Joyce</p>
-              <div className='links'>
-                <a href='https://github.com/gaj23'><img src={github} alt='Gabrielle GitHub' /></a>
-                <a href='https://www.linkedin.com/in/gaj23/'><img src={linkedin} alt='Gabrielle LinkedIn' /></a>
-              </div>
-          </div>
         </div>
+        <h4>To view the original version, you can find it here:</h4>
+          <div className='links'>
+            <a href='https://github.com/conconartist/face-it'><img src={github} alt='FaceIt Repository' /></a>
+          </div>
       <Logo />
     </footer>
   )
